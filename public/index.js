f6f621fb6c99cce86280e5e069c32c81f33f228c
Fuma820 <fuuma820@gmail.com>
2024-04-25 14:15:38 +0900

    ユーザーid認証機能追加

--- public/index.js
+++ public/index.js
@@ -1,14 +1,14 @@
-//ページが完全に読み込まれた時実行
+// ページが完全に読み込まれた時実行
 document.addEventListener("DOMContentLoaded", () => {
-    //認証状態が変更された時に実行
+    // 認証状態が変更された時に実行
     firebase.auth().onAuthStateChanged((user) => {
-        if (user) {//ログインしている場合はmain.htmlにリダイレクト
+        if (user) {// ログインしている場合はmain.htmlにリダイレクト
             window.location.replace("mainPage/main.html");
         }
     });
-    //ログインボタンが押された時実行
+    // ログインボタンが押された時実行
     document.getElementById("login_btn").onclick = () => {
-        //Googleアカウントでログイン
+        // Googleアカウントでログイン
         let provider = new firebase.auth.GoogleAuthProvider();
         firebase.auth().signInWithPopup(provider).then((result) => {
             console.log("User logged in: ", result.user.email);
--- public/mainPage/main.js
+++ public/mainPage/main.js
@@ -9,49 +9,56 @@ const firebaseConfig = {
   measurementId: "G-DFPZ7PDY47"
 };
 
-// Initialize Firebase
 var db = firebase.firestore(firebase.initializeApp(firebaseConfig));
 var uid = null;
-firebase.auth().signInAnonymously();
-firebase.auth().onAuthStateChanged(function (user) {
-  if (user) uid = user.uid;
-});
-
-db.collection("actions").orderBy("createdAt", "desc").limit(1).onSnapshot(function (querySnapshot) {
-  querySnapshot.forEach(function (doc) {
-    var data = doc.data();
-    ref = doc.ref;
-    field = JSON.parse(data.field);
-    selectedX = data.x;
-    selectedY = data.y;
-    currentStone = data.stone;
-    drawField();
-  });
-})
-
 var context = document.querySelector("canvas").getContext("2d");
 var ref = null;
 var field = Array.from(new Array(8), () => new Array(8).fill(0));
 var currentStone = 1;
 var selectedX = 0;
 var selectedY = 0;
-drawField();
+var cellSize = 100;
+// 周囲のマスへの移動を表す配列(例：dx[0], dy[0]は上への移動を表す)
+var dx = [0, 1, 1, 1, 0, -1, -1, -1];
+var dy = [-1, -1, 0, 1, 1, 1, 0, -1];
 
+//ログイン状況が変化した場合呼び出す
+firebase.auth().onAuthStateChanged(function (user) {
+  if (!user) window.location.replace("../index.html");
+  uid = user.uid;
+});
+
+// Initialize Firebase
+db.collection("actions").orderBy("createdAt", "desc").limit(1).onSnapshot(function (querySnapshot) { //最新の情報を参照
+  querySnapshot.forEach(function (doc) {
+    ref = doc.ref;
+    field = JSON.parse(doc.data().field);
+    selectedX = doc.data().x;
+    selectedY = doc.data().y;
+    currentStone = doc.data().stone;
+    drawField();
+  });
+})
+
+// 引数の座標のマスの情報を取得する関数
 function getStone(x, y) {
   if (x < 1 || x > 8 || y < 1 || y > 8) return -1;
   return field[y - 1][x - 1];
 }
 
+// 引数の座標のマスの情報を設定する関数(stone: 0=石がない，1=黒石，2=白石)
 function setStone(x, y, stone) {
   if (x < 1 || x > 8 || y < 1 || y > 8) return;
   field[y - 1][x - 1] = stone;
 }
 
+// 長方形を描画する関数
 function drawRect(color, x, y, w, h) {
   context.fillStyle = color;
   context.fillRect(x, y, w, h);
 }
 
+// 円を描画する関数
 function drawCircle(color, x, y, r) {
   context.fillStyle = color;
   context.beginPath();
@@ -60,71 +67,80 @@ function drawCircle(color, x, y, r) {
   context.fill();
 }
 
+// フィールドを描写する関数
 function drawField() {
-  console.log(selectedX, selectedY)
-  drawRect("white", 0, 0, 800, 800);
+  // ログインしていなければログインページに戻る
+  if (!uid) window.location.replace("../index.html");
+  drawRect("white", 0, 0, cellSize * 8, cellSize * 8);
   for (var y = 1; y <= 8; y++) {
     for (var x = 1; x <= 8; x++) {
       var bgColor = (x == selectedX && y == selectedY) ? "yellow" : "green";
-      drawRect(bgColor, (x - 1) * 100, (y - 1) * 100, 99, 99);
-      if (getStone(x, y) == 1) drawCircle("black", x * 100 - 50, y * 100 - 50, 44);
-      if (getStone(x, y) == 2) drawCircle("white", x * 100 - 50, y * 100 - 50, 44);
+      drawRect(bgColor, (x - 1) * cellSize, (y - 1) * cellSize, cellSize - 1, cellSize - 1);
+      if (getStone(x, y) == 1) drawCircle("black", x * cellSize - cellSize / 2, y * cellSize - cellSize / 2, cellSize * 0.9 / 2);
+      if (getStone(x, y) == 2) drawCircle("white", x * cellSize - cellSize / 2, y * cellSize - cellSize / 2, cellSize * 0.9 / 2);
     }
   }
 }
 
-var dx = [0, 1, 1, 1, 0, -1, -1, -1];
-var dy = [-1, -1, 0, 1, 1, 1, 0, -1];
-function canReverse(stone, x, y) {
+// 引数のマスに石を置けるか判定する関数
+function canPut(stone, x, y) {
   if (getStone(x, y) > 0) return;
+  // 周囲のマスをひっくり返せるか確かめる
   for (var i = 0; i < 8; i++) {
-    if (getReversibleNum(stone, x + dx[i], y + dy[i], dx[i], dy[i], 0) > 0) return true;
+    if (getReversibleNum(stone, x, y, dx[i], dy[i], 0) > 0) return true;
   }
   return false;
 }
 
+// 石を設置し，周囲の石をひっくり返す関数
 function putStone(x, y, stone) {
   setStone(x, y, stone);
   for (var i = 0; i < 8; i++) {
-    if (getReversibleNum(stone, x + dx[i], y + dy[i], dx[i], dy[i], 0) > 0) reverse(stone, x + dx[i], y + dy[i], dx[i], dy[i]);
+    if (getReversibleNum(stone, x, y, dx[i], dy[i], 0) > 0) reverse(stone, x, y, dx[i], dy[i]);
   }
 }
 
+// dx方向にひっくる返せる石の数を返す関数
 function getReversibleNum(stone, x, y, dx, dy, n) {
-  if (getStone(x, y) < 1) return 0;
-  if (getStone(x, y) == stone) return n;
+  if (getStone(x + dx, y + dy) < 1) return 0;
+  if (getStone(x + dx, y + dy) == stone) return n;
   return getReversibleNum(stone, x + dx, y + dy, dx, dy, n + 1);
 }
 
+// 引数の方向の石をひっくり返す関数
 function reverse(stone, x, y, dx, dy) {
-  if (getStone(x, y) < 1) return;
-  if (getStone(x, y) == stone) return;
-  setStone(x, y, stone);
+  if (getStone(x + dx, y + dy) == stone) return;
+  setStone(x + dx, y + dy, stone);
   reverse(stone, x + dx, y + dy, dx, dy);
 }
 
+// 画面をクリックした時の関数
 function onClick(e) {
-  var rect = e.target.getBoundingClientRect()
-  var x = Math.floor((e.clientX - rect.left) / 100) + 1
-  var y = Math.floor((e.clientY - rect.top) / 100) + 1
-  console.log(x, y)
-  if (!canReverse(currentStone, x, y)) return;
+  var rect = e.target.getBoundingClientRect();
+  // クリックされたマスの座標を取得
+  var x = Math.floor((e.clientX - rect.left) / cellSize) + 1;
+  var y = Math.floor((e.clientY - rect.top) / cellSize) + 1;
+
+  if (!canPut(currentStone, x, y)) return;
   selectedX = x;
   selectedY = y;
-  putStone(x, y, currentStone)
+  putStone(x, y, currentStone);
   currentStone = currentStone % 2 + 1;
+  // 置けるマスがあるか判定したい
+
   drawField();
   db.collection("actions").add({
     x: x,
     y: y,
     stone: currentStone,
-    field: JSON.stringify(field),
+    field: JSON.stringify(field), // 配列をJSON形式で保存
     uid: uid,
     prev: ref,
     createdAt: firebase.firestore.FieldValue.serverTimestamp()
   });
 }
 
+// フィールドを初期化する関数
 function init() {
   field = Array.from(new Array(8), () => new Array(8).fill(0));
   currentStone = 1;
@@ -135,6 +151,7 @@ function init() {
   setStone(4, 4, 2);
   setStone(5, 5, 2);
   drawField();
+  // データベースを初期化
   db.collection("actions").add({
     x: selectedX,
     y: selectedY,
@@ -145,3 +162,5 @@ function init() {
     createdAt: firebase.firestore.FieldValue.serverTimestamp()
   });
 }
+
+// ログアウト機能
